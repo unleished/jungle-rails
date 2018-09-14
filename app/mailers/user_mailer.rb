8cfa1b81d80e8ca2015e9ab7d9e71ef381edc954
Leisha Magsisi <leisha.gallagher@gmail.com>
2018-09-14 14:30:29 +0000

    updating css and look of page

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
   default from: "no-reply@jungle.com"
 
-  def order_email (user, order)
+  def order_email(user, order)
     @user = user
     @order = order
 
